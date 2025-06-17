51b0fdd89bfdf74b910f23d42a601c2854910ed5
korneevscp <vladimirivanof2024@gmail.com>
2025-06-17 16:41:14 +0200

    Task 4.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main : Entry point
+ * main - puts a string with special character.
  *
  * Description: Prints a message using puts
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
