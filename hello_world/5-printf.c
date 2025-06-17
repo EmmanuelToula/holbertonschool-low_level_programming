0d0e932f92a2f1743b062cccfc5f19bdb5e7731b
korneevscp <vladimirivanof2024@gmail.com>
2025-06-17 17:14:08 +0200

    Task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a sentence with a new line
+ *
+ * 
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
